3a08bf929e888c5bd35074cba58a18f1ca6cfefa
Victor <davik4life@gmail.com>
2026-01-18 22:21:51 +0100

    Save local changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView, register
-from django.contrib.auth import views as auth_views
+# from django.urls import path
+# from .views import list_books, LibraryDetailView, LoginView, LogoutView, register
+# from django.contrib.auth import views as auth_views
 
 # urlpatterns = [
 #     path("books/", list_books, name="list_books"),
@@ -14,8 +14,27 @@ from django.contrib.auth import views as auth_views
 #     path("Libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 # ]
 
+# urlpatterns = [
+#     # Authentication
+#     path("login/", auth_views.LoginView.as_view(
+#         template_name="relationship_app/login.html"
+#     ), name="login"),
+
+#     path("logout/", auth_views.LogoutView.as_view(
+#         template_name="relationship_app/logout.html"
+#     ), name="logout"),
+
+#     path("register/", register, name="register"),
+# ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import list_books, LibraryDetailView, register
+
 urlpatterns = [
-    # Authentication
+    path("books/", list_books, name="list-books"),
+    path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
+
     path("login/", auth_views.LoginView.as_view(
         template_name="relationship_app/login.html"
     ), name="login"),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,99 +1,19 @@
-from django.shortcuts import render, redirect
-# from relationship_app.models import Book, Author, Librarian, Library
-from django.views.generic.detail import DetailView
-from .models import Library, Book
-from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Create your views here.
-# import using FBV Function Based View
-
-# How I did it before
-
-# def query_all_books(request, title):
-#     books = Book.objects.all(name = title)
-#     authors = Author.objects.all()
-#     return render(request, 'books.html', {'books': books, 'authors': authors})
-
-# Chat GPT's help and provided code
-@login_required
-def list_books(request):
-    books = Book.objects.all()
-
-    context = {
-        "books": books
-    }
-
-    return render(request, "relationship_app/list_books.html", context)
-
-
-# Views for register
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("list_books")
+            return redirect("/")
     else:
         form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
 
-# Views for login
-
-def login(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("list_books")
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
-
-# Views for logout
-
-def logout(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("list_books")
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/logout.html", {"form": form})
-
-
-
-# def query_books_by_author(request, author_name):
-#     books = Book.objects.filter(author__name=author_name)
-#     authors = Author.objects.all()
-#     return render(request, 'books.html', {'books': books, 'authors': authors})
-
-# Using CBV Class based View
-
-@LoginRequiredMixin
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
-
-
-class LoginView(DetailView):
-    model = Library
-    template_name = "relationship_app/login.html"
-    context_object_name = "library"
-
-class LogoutView(DetailView):
-    model = Library
-    template_name = "relationship_app/logout.html"
-    context_object_name = "library"
-
-class RegisterView(DetailView):
-    model = Library
-    template_name = "relationship_app/register.html"
-    context_object_name = "library"
+    return render(
+        request,
+        "relationship_app/register.html",
+        {"form": form}
+    )
