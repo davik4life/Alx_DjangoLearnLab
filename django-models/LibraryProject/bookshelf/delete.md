3817ed8b50c6ca5da77e8553192361bbfc856bf6
Victor <davik4life@gmail.com>
2026-01-18 19:10:43 +0100

    Duplicated the Project Directory for Django models

---
+++ django-models/LibraryProject/bookshelf/delete.md
@@ -0,0 +1,15 @@
+
+---
+
+## `delete.md`
+
+```md
+# Delete Book Instance
+
+```python
+from bookshelf.models import Book
+
+book = Book.objects.get(title="Nineteen Eighty-Four")
+book.delete()
+
+Book.objects.all()
---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    pass
+
+class Book(models.Model):
+    pass
+
+class Library(models.Model):
+    pass
+
+class Librarian(models.Model):
+    pass
+
+
