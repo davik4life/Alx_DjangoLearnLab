9d61af73000fcf0fa85123f719cda83ce3c3ee42
Victor <davik4life@gmail.com>
2026-01-18 19:24:14 +0100

    fixes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) 
+    def get_name(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
@@ -14,6 +16,4 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library)
-
-
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
