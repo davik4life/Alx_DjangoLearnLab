d363084f112a6fd0f589cf83584f5022195366b8
Victor <davik4life@gmail.com>
2026-01-24 15:36:03 +0100

    Swithced to the Bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
@@ -6,3 +7,35 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.PositiveIntegerField()
+
+# Create a Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+# Define a custom ModelAdmin class that includes configurations for the additional fields in your user model.
+class ModelAdmin()
+
+# Custom Manager Functions to Ensure it handles the new fields correctly
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password, date_of_birth=None, profile_photo=None):
+        user = self.model(
+            username=username,
+            email=email,
+            password=password,
+        date_of_birth=date_of_birth,
+        profile_photo=profile_photo
+    )
+        return user
+
+    def create_superuser(self, username, email, password, date_of_birth=None, profile_photo=None):
+        user = self.model(
+            username=username,
+            email=email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo
+        )
+        return user
