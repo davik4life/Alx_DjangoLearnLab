112387b637a65764aacb57f8201920e6fe25ff36
Victor <davik4life@gmail.com>
2026-01-24 19:48:32 +0100

    Add up

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 # Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        author = request.POST["author"]
+        Book.objects.create(title=title, author=author)
+        return redirect("book_list")
+    return render(request, "create_book.html")
+
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == "POST":
+        book.title = request.POST["title"]
+        book.author = request.POST["author"]
+        book.save()
+        return redirect("book_list")
+    return render(request, "edit_book.html", {"book": book})
