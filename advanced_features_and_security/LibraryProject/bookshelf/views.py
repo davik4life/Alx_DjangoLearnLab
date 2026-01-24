421b7b5669e6b2c89e2aa9ca269ad7be381b373f
Victor <davik4life@gmail.com>
2026-01-24 20:47:42 +0100

    Added again and again

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
+
 # Create your views here.
 
 @permission_required("bookshelf.can_view", raise_exception=True)
