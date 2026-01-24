2e43411a6e565f94175d4508be8114564e307fb7
Victor <davik4life@gmail.com>
2026-01-24 15:31:45 +0100

    Added all that's added to be added

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
 # Create your models here.
 class Author(models.Model):
@@ -25,8 +25,8 @@ class Book(models.Model):
         return self.title
 
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
@@ -64,3 +64,35 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+# Create a Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+# Define a custom ModelAdmin class that includes configurations for the additional fields in your user model.
+class ModelAdmin()
+
+# Custom Manager Functions to Ensure it handles the new fields correctly
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password, date_of_birth=None, profile_photo=None):
+        user = self.model(
+            username=username,
+            email=email,
+            password=password,
+        date_of_birth=date_of_birth,
+        profile_photo=profile_photo
+    )
+        return user
+
+    def create_superuser(self, username, email, password, date_of_birth=None, profile_photo=None):
+        user = self.model(
+            username=username,
+            email=email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo
+        )
+        return user
