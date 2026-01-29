db6352f8710630ee3f24e5b7eec767c47e8b1ca3
Victor <davik4life@gmail.com>
2026-01-29 11:52:26 +0100

    Changes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
-    path('books/', BookViewSet.as_view(), name='books'),
+    # path('books/', BookViewSet.as_view(), name='books'),
+    path(DefaultRouter(), BookViewSet.as_view(), name='default'),
 ]
