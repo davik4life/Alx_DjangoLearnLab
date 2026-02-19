b76c77645c0ff1743e54a2af5cdb74ec23bf528a
Victor <davik4life@gmail.com>
2026-02-19 16:52:35 +0100

    Utilized the get_user_model()

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from .models import User
 from rest_framework.authtoken.models import Token
+User = get_user_model()
 
 # Serializers for User model
 class UserSerializer(serializers.ModelSerializer):
@@ -18,7 +19,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
