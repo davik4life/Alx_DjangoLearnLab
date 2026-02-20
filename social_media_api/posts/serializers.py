f8b5ad63507e0655a1990e4f2e51bd54300c9a3c
Victor <davik4life@gmail.com>
2026-02-20 16:44:11 +0100

    Refactored my serializers.py file

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import *
 from accounts.serializers import UserSerializer
-from accounts.models import User
 from rest_framework.authtoken.models import Token
 
 
