dc95d6bf1edbd9d081dddeda09db6d0a4eb6caaf
Victor <davik4life@gmail.com>
2026-02-20 17:02:04 +0100

    Removed .models import from views.py as it is redundant

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from .models import *
 from .serializers import *
 from .permissions import IsAuthorOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
