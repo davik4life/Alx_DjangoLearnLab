0a8213fe96b93fc7bdb19f132ed70c488d53f8a9
Victor <davik4life@gmail.com>
2026-02-22 03:20:32 +0100

    Fix APIView import

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, APIView
+from rest_framework import viewsets
+from rest_framework.views import APIView
 from .serializers import *
 from .permissions import IsAuthorOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
